0ed6bac0ee696a131f2b9915d291fd25d23f44c1
Darrah K <10011911+llamawithmemoryloss@users.noreply.github.com>
2018-07-30 08:43:28 +0100

    cleaning up

--- Network.py
+++ Network.py
@@ -4,6 +4,8 @@
 # External lib
 import numpy as np
 
+# TESTING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+np.random.seed(0)
 
 class Network:
 
@@ -23,6 +25,7 @@ class Network:
         # Clearing all the data so it can be used for feeding forward.
         self.a_data = []
         self.nodes = []
+        # We need the zs to be stored so we can use it in back propagation.
         self.zs = []
         self.a_data = np.matrix(a_data).transpose()
         self.nodes.append(self.a_data)
@@ -42,9 +45,9 @@ class Network:
         self.feed_forward(x)
         # Working out the first propagation.
         y_tran = np.matrix(y).transpose()
-        delta = np.multiply(cost(self, y_tran, True), sigmoid(self.zs[-1], True))
-        biases_copy = np.copy(self.biases)
-        weights_copy = np.copy(self.weights)
+        delta = np.multiply(cost(self.nodes[-1], y_tran, True), sigmoid(self.zs[-1], True))
+        biases_copy = [np.zeros(b.shape) for b in self.biases]
+        weights_copy = [np.zeros(w.shape) for w in self.weights]
         biases_copy[-1] = delta
         nodes_2 = np.matrix(self.nodes[-2])
         weights_copy[-1] = np.dot(delta, nodes_2.transpose())
@@ -57,29 +60,6 @@ class Network:
             weights_copy[-back-1] = np.dot(delta, nodes.transpose())
 
         return biases_copy, weights_copy
-
-    def collect_start(self):
-        """ Collects the network structure, biases, and weights of the network and stores it in a cvs file.
-        """
-        with open("Starting Data.csv", "w") as f:
-            f.write("Network structure, " + ", ".join(str(size) for size in self.sizes) + "\n")
-            f.write("Biases, " + ", ".join(str(biases.transpose()[0]) for biases in self.biases))
-            for layer in range(self.layers - 1):
-                f.write("\nWeights for layer " + str(layer) + ", "
-                        + ", ".join(str(self.weights[layer][i]) for i in range(self.sizes[layer] + 1)))
-            f.close()
-
-    def collect_all(self):
-        """ Collects the network structure, biases, weights, and nodes of the networks and stores it in a csv file.
-        """
-        with open("All Data.csv", "w") as f:
-            f.write("Network structure, " + ", ".join(str(size) for size in self.sizes) + "\n")
-            f.write("Biases, " + ", ".join(str(biases.transpose()[0]) for biases in self.biases))
-            for layer in range(self.layers - 1):
-                f.write("\nWeights for layer " + str(layer) + ", "
-                        + ", ".join(str(self.weights[layer][i]) for i in range(self.sizes[layer] + 1)))
-            f.write("\nNodes, " + ", ".join(str(np.array(node.transpose())[0]) for node in self.nodes))
-            f.close()
 
 
 # Other functions that are needed.
@@ -94,7 +74,7 @@ def sigmoid(x, prime=False):
     return 1 / (1 + np.exp(-x))
 
 
-def cost(self, correct_data, prime=False):
+def cost(output, correct_data, prime=False):
     """ Works out the cost/cost prime of each element of the output of the network
     :param self: This is used to get the get the last layer of the nodes of the network
     :param correct_data: The output value as a np.matrix column of the correct value for the input.
@@ -102,18 +82,8 @@ def cost(self, correct_data, prime=False):
     :return: This will return a np.matrix of cost/cost prime function applied to every element of the np.matrix.
     """
     if prime:
-        return 2 * np.subtract(self.nodes[-1], correct_data)
-    return np.sum(np.square(np.subtract(self.nodes[self.layers - 1], correct_data)))
+        return 2 * np.subtract(output, correct_data)
+    return np.sum(np.square(np.subtract(output, correct_data)))
 
-<<<<<<< HEAD
-if __name__ == "__main__":
-    "This is used for testing of the Network and has nothing to do with the networks functionality."
-    A = Network([2,3,1])
-    A.feed_forward([1,2])
-    print(A.weights)
-    print(A.biases)
-    print(A.nodes)
-    A.back_prop([1,2],[1])
 
-=======
->>>>>>> 019f9c0084b93b9c0a9bf5a6b4884b370e960df8
+
